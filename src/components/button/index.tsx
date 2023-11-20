cda179bc5c87b9e4b857ef6c61bf6df0349cb897
yasaman <yasamanbarati81@gmail.com>
2023-11-20 12:38:52 +0330

    improvment code structure (component)

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,9 +1,11 @@
 "use client";
 
 import { Button, styled } from "@mui/material"
+import { toast } from "react-toastify";
 
 interface Props {
     text: string,
+    isMark?: boolean
 }
 
 const ButtonBox = styled(Button)(({ theme }) => ({
@@ -13,12 +15,30 @@ const ButtonBox = styled(Button)(({ theme }) => ({
     transition: '0.5s',
     textAlign: 'center',
 }))
-export const ReactiveButton = ({ text }: Props) => {
+export const ReactiveButton = ({ text, isMark }: Props) => {
+
+    const notify = () => {
+        if (isMark === false) {
+            toast.warn('username and password are incorrect! try again', {
+                position: "top-left",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "light",
+            });
+        }
+        else return
+    }
+
     return (
         <ButtonBox
             type="submit"
             fullWidth
             variant="contained"
+            onClick={notify}
             sx={{ mt: 3, mb: 2 }}>{text}</ButtonBox>
     )
 }
--- src/components/input_field/index.tsx
+++ src/components/input_field/index.tsx
@@ -1,14 +1,21 @@
 "use client";
 
 import { styled, TextField } from "@mui/material";
+import { ChangeEvent } from "react";
 
 interface Props {
     label: string,
-    name: string,
     autoComplete: string,
+    value: string,
+    onChange: (e: ChangeEvent<HTMLInputElement>) => void
 }
 
 const TextFieldBox = styled(TextField)(({ theme }) => ({
+    "& .MuiInputBase-root.MuiOutlinedInput-root:hover": {
+        "& fieldset": {
+            borderColor: theme.palette.blue.light,
+        },
+    },
     "& fieldset": {
         border: `1.5px solid ${theme.palette.blue.light}`,
         borderRadius: "40px",
@@ -17,17 +24,17 @@ const TextFieldBox = styled(TextField)(({ theme }) => ({
 
 
 
-export const InputField = ({ label, name, autoComplete }: Props) => {
+export const InputField = ({ label, autoComplete, value, onChange }: Props) => {
     return (
         <TextFieldBox
             margin="normal"
             required
             fullWidth
-            id={label}
             label={label}
-            name={name}
             autoComplete={autoComplete}
             autoFocus={false}
+            value={value}
+            onChange={onChange}
         />
     )
 }
